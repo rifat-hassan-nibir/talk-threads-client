bc33191e561aa6743cfe97dbb438facd8ef6377c
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-06 08:51:36 +0600

    added scroll restoration to mainlayout and icons to single post page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <ScrollRestoration />
       <Navbar></Navbar>
       <div className=" min-h-[calc(100vh-136px)]">
         <Outlet></Outlet>
