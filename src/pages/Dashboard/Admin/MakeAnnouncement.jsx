6f45257606b66051f476e4ad63022beb3c6189a9
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-08 16:52:03 +0600

    some pages added

---
+++ src/pages/Dashboard/Admin/MakeAnnouncement.jsx
@@ -0,0 +1,9 @@
+const MakeAnnouncement = () => {
+  return (
+    <div>
+      <h1>Make Announcement Page</h1>
+    </div>
+  );
+};
+
+export default MakeAnnouncement;
---
+++ src/pages/Dashboard/Admin/ManageUser.jsx
@@ -0,0 +1,9 @@
+const ManageUser = () => {
+  return (
+    <div>
+      <h1>Manage Users</h1>
+    </div>
+  );
+};
+
+export default ManageUser;
